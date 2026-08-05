37d5643f502ca66df61b0d9608e270e961fd3451
ParkSuah777 <parksuah673@gmail.com>
2026-08-05 19:07:00 +0700

    Add React frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Link, Route, Routes } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <>
+ 
+      <Routes>
+     
+        <Route path="/" element={}></Route>
+       
+      </Routes>
+    </>
+  );
+}
+
+export default App;
